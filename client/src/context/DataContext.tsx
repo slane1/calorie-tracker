81953f2b655b9f2a388fc4a5bf8f6eb9a99b9dd3
Stefan Lange <stefanlange@maeve.de>
2024-06-08 23:28:09 +0200

    data context boiler

--- client/src/context/DataContext.tsx
+++ client/src/context/DataContext.tsx
@@ -1,3 +1,22 @@
-export default function DataContext() {
-    
+import { createContext, useState, ReactNode, FC } from "react";
+export const DataContext = createContext<DataContextType | null>(null);
+
+interface DataContextType {
+    loading: boolean;
+    setLoading: (loading: boolean) => void;
 }
+interface DataContextProviderProps {
+    children: ReactNode;
+}
+
+const DataContextProvider: FC<DataContextProviderProps> = ({ children }) => {
+    const [loading, setLoading] = useState<boolean>(false);
+
+    return (
+        <DataContext.Provider value={{ loading, setLoading }}>
+            {children}
+        </DataContext.Provider>
+    );
+}
+
+export default DataContextProvider;
