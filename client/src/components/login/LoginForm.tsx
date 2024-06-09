52ce9fd47e5d0fdf5adb1b395bed59d208c53479
Stefan Lange <stefanlange@maeve.de>
2024-06-10 01:41:47 +0200

    login form

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -1,3 +1,46 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+
 export default function LoginForm() {
-    
+    const [loginForm, setLoginForm] = useState({
+        username: '',
+        pass: ''
+    });
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        const { name, value } = e.target;
+        setLoginForm(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        // add submission api logic
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username</label>
+                <input 
+                    type="text" 
+                    name="username" 
+                    value={loginForm.username} 
+                    onChange={handleChange} 
+                />
+                <label htmlFor="pass">Password</label>
+                <input 
+                    type="password" 
+                    name="pass" 
+                    value={loginForm.pass} 
+                    onChange={handleChange} 
+                />
+                <button type="submit">Login</button>
+            </form>
+            <div>
+                <p>Don't have an Account?</p><p>Register now</p>
+            </div>
+        </div>
+    )
 }
